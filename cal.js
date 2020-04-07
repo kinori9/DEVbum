87f88eea42cf3578f288d38b3dec74b97454010a
kinori9 <50355001+kinori9@users.noreply.github.com>
2020-04-08 00:18:30 +0900

    계산기 추가
    제목이 곧 내용

---
+++ cal.js
@@ -0,0 +1,17 @@
+const form = document.querySelector(".calculator");
+const w50000 = document.getElementById("1");
+const w10000 = document.getElementById("2");
+const w5000 = document.getElementById("3");
+const w1000 = document.getElementById("4");
+const w500 = document.getElementById("5");
+const w100 = document.getElementById("6");
+const result = document.querySelector(".result");
+const NONE = "none";
+
+function calculator(){
+    const value = w50000.value*50000+w10000.value*10000+w5000.value*5000+w1000.value*1000+w500.value*500+w100.value*100;
+    form.classList.add(NONE);
+    result.innerText=`총 ${value}원 입니다.`;
+    
+    
+}
