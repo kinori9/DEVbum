82412ccb721ab125f78a7da0e2d2bf0f10976a6b
kinori9 <50355001+kinori9@users.noreply.github.com>
2020-04-08 01:16:50 +0900

    주소 추가

--- cal.js
+++ cal.js
@@ -6,12 +6,21 @@ const w1000 = document.getElementById("4");
 const w500 = document.getElementById("5");
 const w100 = document.getElementById("6");
 const result = document.querySelector(".result");
+const changeMoneyShop = document.querySelector(".changeMoneyShop");
 const NONE = "none";
+const SHOWING_CN = "showing";
 
 function calculator(){
     const value = w50000.value*50000+w10000.value*10000+w5000.value*5000+w1000.value*1000+w500.value*500+w100.value*100;
     form.classList.add(NONE);
     result.innerText=`총 ${value}원 입니다.`;
+}
+function changeMoney(){
+    if(changeMoneyShop.className == "changeMoneyShop"){
+        changeMoneyShop.classList.add(SHOWING_CN);
+    }else if(changeMoneyShop.className == "changeMoneyShop "+SHOWING_CN){
+        changeMoneyShop.classList.remove(SHOWING_CN);
+    }
     
     
 }
